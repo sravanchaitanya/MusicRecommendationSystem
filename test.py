948a663f97038e4a4b1e320fa2dfbf4f26455d85
Pothana Sravan Chaitanya <79583589+sravanchaitanya@users.noreply.github.com>
2024-05-05 01:01:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,33 +1,44 @@
 import streamlit as st
-from sklearn.datasets import fetch_california_housing 
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
-from sklearn.model_selection import  train_test_split
-from sklearn.ensemble import RandomForestRegressor
-st.header('This is my first app')
-st.text('Polayadi mone')
-housing = fetch_california_housing()
-df = pd.DataFrame(housing.data,columns = housing.feature_names)
-df['target']=housing .target
-st.write(df.head)
-st.write(df.shape)
-st.write(df.columns)
-st.text("Information about the dataset - missing values")
-st.write(df.isnull().sum())
+import numpy as np
+import requests
+import builtins
+from PIL import Image
+from io import BytesIO
 
-chart = st.container()
-model = st.container()
-predict = st.container()
+top100 = pd.read_csv('C:/Users/srava/Desktop/Final_Project/data/top100.csv')
 
-with chart:
-    st.text("This is inside container")
-    st.bar_chart(df['Population'])
-    st.area_chart(df['Latitude'])
-    st.line_chart(df['HouseAge'])
-    st.scatter_chart(df['target'])
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="6239ea24c3d64de2a09122ee3d157247",
+                                                           client_secret="d4c17019488349318a3776b72f60b2aa"))
 
-with model:
-    X = df.drop("target", axis=1)  # Replace "target_column" with actual name
-    y = df["target"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = RandomForestRegressor()  # Replace with your model
-    model.fit(X_train,y_train)
+
+
+def get_image(name,year):
+    result = sp.search(q = 'name: {} year: {}'.format(name,year))
+    if not result['tracks']['items']==[]:
+        url= result['tracks']['items'][0]['album']['images'][0]['url']
+        return Image.open(BytesIO(requests.get(url).content))
+    else:
+        url= 'default.png'
+        return Image.open(url)
+
+images=[]
+
+for i in range(0,100):
+    images.append(get_image(top100['name'][i],top100['year'][i]))
+    
+containers = []
+
+columns = st.columns(3)
+
+for i in range(0,100):
+    with columns[i%3]:
+        container = st.container()
+        containers.append(container)
+        with container:
+            st.image(images[i])
+            st.write(top100['name'][i])
+            flag = st.button('Select',key=i)
+            
